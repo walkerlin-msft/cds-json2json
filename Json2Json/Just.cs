9be7729927f63d0540e3252cd6c2be9acb5205f0
Walker Lin (MSFT) <a-walin@microsoft.com>
2017-11-20 19:13:28 +0800

    JSON to JSON

---
+++ Json2Json/Just.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json2Json
+{
+    /* 
+     * JUST Stands for JSON Under Simple Transformation
+     * https://github.com/WorkMaze/JUST.net
+     * */
+
+    public class Just
+    {
+        public static string CDS_MESSAGE_TRANSFORM_PROPERTY_NOT_FOUND = "CDS_MESSAGE_TRANSFORM_PROPERTY_NOT_FOUND";
+
+        public static string GetValue(string key)
+        {
+            return "#valueof($." + key + ")";
+        }
+
+        public static string GetValueIfExists(string key)
+        {
+            return IfCondition(Exists(key),
+                "true",
+                GetValue(key),
+                CDS_MESSAGE_TRANSFORM_PROPERTY_NOT_FOUND);
+        }
+
+        public static string GetValueIfExistsAndNotEmpty(string key)
+        {
+            return IfCondition(ExistsAndNotEmpty(key),
+                "true",
+                GetValue(key),
+                CDS_MESSAGE_TRANSFORM_PROPERTY_NOT_FOUND);
+        }
+
+        public static string IfCondition(string condition, string evaluation, string trueResult, string falseResult)
+        {
+            return "#ifcondition(" + condition + "," + evaluation + "," + trueResult + "," + falseResult + ")";
+        }
+
+        public static string GetLastIndexOf(string input, string search)
+        {
+            return "#lastindexof(" + input + "," + search + ")";
+        }
+        public static string GetFastIndexOf(string input, string search)
+        {
+            return "#firstindexof(" + input + "," + search + ")";
+        }
+
+        public static string Add(string valueA, string valueB)
+        {
+            return "#add(" + valueA + "," + valueB + ")";
+        }
+
+        public static string Subtract(string valueA, string valueB)
+        {
+            return "#subtract(" + valueA + "," + valueB + ")";
+        }
+
+        public static string Concat(string s1, string s2)
+        {
+            return "#concat(" + s1 + "," + s2 + ")";
+        }
+
+        public static string SubString(string input, string startIndex, string length)
+        {
+            return "#substring(" + input + "," + startIndex + "," + length + ")";
+        }
+
+        public static string ContainsOfString(string input, string pattern)
+        {
+            // Return "true" or "false"
+            return IfCondition(GetFastIndexOf(input, pattern),
+                "-1",
+                "false",
+                "true");
+        }
+
+        public static string GetStringLength(string input)
+        {
+            return GetLastIndexOf(Concat(input, ":"), ":");
+        }
+
+        public static string Exists(string key)
+        {
+            // Return "true" or "false"
+            return "#exists($." + key + ")";
+        }
+
+        public static string ExistsAndNotEmpty(string key)
+        {
+            // Return "true" or "false"
+            return "#existsandnotempty($." + key + ")";
+        }
+
+        public static string SubStringFromLastColon(string input)
+        {
+            string startIndex = Add(GetLastIndexOf(input, ":"), "1");
+
+            string totalLength = GetStringLength(input);
+
+            string length = Subtract(totalLength, startIndex);
+
+            return SubString(input, startIndex, length);
+        }
+    }
+}
---
+++ Json2Json/Program.cs
@@ -0,0 +1,142 @@
+﻿using JUST;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json2Json
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            testOPCUA();
+            //testInputSampleFromFile();
+
+            Console.ReadLine();
+        }
+
+        static void testInputSampleFromFile()
+        {
+            string input = File.ReadAllText("../../Example/Input.json");
+
+            string transformer = File.ReadAllText("../../Example/Transformer_copy.json");
+
+            string transformedString = JsonTransformer.Transform(transformer, input);
+            Console.WriteLine("################################################################################################");
+            Console.WriteLine(transformedString);
+        }
+
+        static void testOPCUA()
+        {
+            string input = getOPCUAinput();
+
+            Console.WriteLine("input= {0}\n", input);
+
+            string transformer = getOPCUATransformer(69, 84);
+
+            Console.WriteLine("transformer= {0}\n", transformer);
+
+            // DO THE TRANSFORM
+            string output = JsonTransformer.Transform(transformer, input);
+            Console.WriteLine("output= {0}", output);
+
+            // Save the JSON file
+            var filePath = @".\transformer.json";
+            File.WriteAllText(filePath, transformer);
+            Console.WriteLine("filePath= {0}", filePath);
+        }
+
+        static string getOPCUAinput()
+        {
+            var inputJson = new JObject();
+            var valueJson = new JObject();
+
+            valueJson.Add("Value", 2717);
+            //valueJson.Add("Value", null);
+
+            valueJson.Add("SourceTimestamp", "2017-11-04T09:59:48.3407981Z");
+            //valueJson.Add("SourceTimestamp", "");
+
+            //inputJson.Add("ApplicationUri", "urn:OPC-A:ICPDAS:ICPDAS_OPC_UA_ServerABC");
+            inputJson.Add("ApplicationUri", "urn:OPC-A:ICPDAS:ICPDAS_OPC_UA_ServerA01");
+            //inputJson.Add("ApplicationUri", "urn:OPC-A:ICPDAS:ICPDAS_OPC_UA_ServerA02");
+
+            inputJson.Add("DisplayName", "ns=2;s=MTCP_CurrentValueTask.Temperature_C");
+
+            //inputJson.Add("NodeId", "ns=2;s=MTCP_CurrentValueTask.Temperature_F");
+            inputJson.Add("NodeId", "ns=2;s=MTCP_CurrentValueTask.Temperature_C");
+
+            inputJson.Add("Value", valueJson);
+
+            return inputJson.ToString();
+        }
+
+        static string getOPCUATransformer(int companyId, int messageCatalogId)
+        {
+            var transformerJson = new JObject();
+
+            // messageCatalogId
+            transformerJson.Add("messageCatalogId", messageCatalogId);
+
+            // companyId
+            transformerJson.Add("companyId", companyId);
+
+            // msgTimestamp
+            var msgTimestamp = Just.GetValueIfExistsAndNotEmpty("Value.SourceTimestamp");
+            transformerJson.Add("msgTimestamp", msgTimestamp);
+
+            // equipmentId
+            var applicationUri = Just.GetValueIfExists("ApplicationUri");
+
+            var equipmentId = Just.IfCondition(applicationUri,
+                "urn:OPC-A:ICPDAS:ICPDAS_OPC_UA_ServerA01",
+                "A001",
+                Just.IfCondition(applicationUri,
+                    "urn:OPC-A:ICPDAS:ICPDAS_OPC_UA_ServerA02",
+                    "A002",
+                    Just.SubStringFromLastColon(applicationUri)));
+            transformerJson.Add("equipmentId", equipmentId);
+
+            // equipmentRunStatus
+            transformerJson.Add("equipmentRunStatus", 1);// Hard code
+
+
+            var valueValue = Just.GetValueIfExistsAndNotEmpty("Value.Value");
+            var nodeId = Just.GetValueIfExistsAndNotEmpty("NodeId");
+
+            // Temperature
+            var temperature = Just.IfCondition(
+                nodeId,
+                Just.CDS_MESSAGE_TRANSFORM_PROPERTY_NOT_FOUND,
+                Just.CDS_MESSAGE_TRANSFORM_PROPERTY_NOT_FOUND,
+                Just.IfCondition(
+                    nodeId,
+                    "ns=2;s=MTCP_CurrentValueTask.Temperature_C",
+                    valueValue,
+                    Just.CDS_MESSAGE_TRANSFORM_PROPERTY_NOT_FOUND));
+            transformerJson.Add("Temperature", temperature);
+
+            // TemperatureF
+            var temperatureF = Just.IfCondition(
+                nodeId,
+                Just.CDS_MESSAGE_TRANSFORM_PROPERTY_NOT_FOUND,
+                Just.CDS_MESSAGE_TRANSFORM_PROPERTY_NOT_FOUND,
+                Just.IfCondition(
+                    nodeId,
+                    "ns=2;s=MTCP_CurrentValueTask.Temperature_F",
+                    valueValue,
+                    Just.CDS_MESSAGE_TRANSFORM_PROPERTY_NOT_FOUND));
+            transformerJson.Add("TemperatureF", temperatureF);
+
+            return transformerJson.ToString();
+        }
+    }
+}
